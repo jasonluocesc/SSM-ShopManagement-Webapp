16eb122a1de5afe327951adc55ffbdc334ddae65
jasonluocesc <jasonluocesc@gmail.com>
2018-07-02 13:34:15 +0300

    add logback configuration

--- src/test/java/com/wenbin/o2o/service/AreaServiceTest.java
+++ src/test/java/com/wenbin/o2o/service/AreaServiceTest.java
@@ -2,7 +2,10 @@ package com.wenbin.o2o.service;
 
 import com.wenbin.o2o.BaseTest;
 import com.wenbin.o2o.entity.Area;
+import com.wenbin.o2o.web.superadmin.AreaController;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -13,6 +16,7 @@ import static org.junit.Assert.*;
  * @author Wenbin Luo @ Aalto University
  */
 public class AreaServiceTest extends BaseTest {
+
 
     @Autowired
     private AreaService areaService;
